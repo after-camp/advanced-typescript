013a4d503422cc2985332aacfe0fb6e9be616b52
Junsuk Park <park64kr63@gmail.com>
2023-07-31 10:20:59 +0900

    misc

--- src/chapter3-advanced-pattern/06-identity-functions/30-no-generics-on-objects.problem.ts
+++ src/chapter3-advanced-pattern/06-identity-functions/30-no-generics-on-objects.problem.ts
@@ -1,19 +1,8 @@
-/**
- * fetchers is an object where you can optionally
- * pass keys that match the route names.
- *
- * BUT - how do we prevent the user from passing
- * fetchers that don't exist in the routes array?
- *
- * We'll need to change this to a function which takes
- * in the config as an argument.
- *
- * Desired API:
- *
- * const config = makeConfigObj(config);
- */
+const makeConfigObj = (config: unknown) => {
+  return config;
+}
 
-export const configObj = {
+export const configObj = makeConfigObj({
   routes: ["/", "/about", "/contact"],
   fetchers: {
     // @ts-expect-error
@@ -21,4 +10,4 @@ export const configObj = {
       return {};
     },
   },
-};
+});
